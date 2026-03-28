32610fb54605cfdf9371e78583983099fb3d4b02
Jairon Alves Lima <jairon.alves.lima@gmail.com>
2026-03-28 16:44:23 -0300

    feat: Redis Streams

---
+++ src/Orleans.Investimentos.Streaming/Redis/Hosting/RedisStreamProviderBuilder.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Orleans;
+using Orleans.Hosting;
+using Orleans.Investimentos.Streaming.Redis.Hosting;
+using Orleans.Providers;
+using StackExchange.Redis;
+
+[assembly: RegisterProvider("Redis", "Streaming", "Silo", typeof(RedisStreamProviderBuilder))]
+[assembly: RegisterProvider("AzureRedisCache", "Streaming", "Silo", typeof(RedisStreamProviderBuilder))]
+[assembly: RegisterProvider("Redis", "Streaming", "Client", typeof(RedisStreamProviderBuilder))]
+[assembly: RegisterProvider("AzureRedisCache", "Streaming", "Client", typeof(RedisStreamProviderBuilder))]
+
+namespace Orleans.Investimentos.Streaming.Redis.Hosting;
+
+internal sealed class RedisStreamProviderBuilder : IProviderBuilder<ISiloBuilder>, IProviderBuilder<IClientBuilder>
+{
+    public void Configure(ISiloBuilder builder, string name, IConfigurationSection configurationSection)
+    {
+        builder.AddRedisStreams(name, streamsBuilder =>
+        {
+            streamsBuilder.ConfigureRedis(optionsBuilder => optionsBuilder.Configure<IServiceProvider>((options, services) =>
+            {
+                var serviceKey = configurationSection["ServiceKey"];
+                if (!string.IsNullOrEmpty(serviceKey))
+                {
+                    // Get a connection multiplexer instance by name.
+                    var multiplexer = services.GetRequiredKeyedService<IConnectionMultiplexer>(serviceKey);
+                    options.CreateMultiplexer = _ => Task.FromResult(multiplexer);
+                    options.ConfigurationOptions = new ConfigurationOptions();
+                }
+                else
+                {
+                    // Construct a connection multiplexer from a connection string.
+                    var connectionName = configurationSection["ConnectionName"];
+                    var connectionString = configurationSection["ConnectionString"];
+                    if (!string.IsNullOrEmpty(connectionName) && string.IsNullOrEmpty(connectionString))
+                    {
+                        var rootConfiguration = services.GetRequiredService<IConfiguration>();
+                        connectionString = rootConfiguration.GetConnectionString(connectionName);
+                    }
+
+                    if (!string.IsNullOrEmpty(connectionString))
+                    {
+                        options.ConfigurationOptions = ConfigurationOptions.Parse(connectionString);
+                    }
+                }
+            }));
+
+            if (int.TryParse(configurationSection["PartitionCount"], out var partitionCount))
+            {
+                streamsBuilder.ConfigurePartitioning(partitionCount);
+            }
+        });
+    }
+
+    public void Configure(IClientBuilder builder, string name, IConfigurationSection configurationSection)
+    {
+        builder.AddRedisStreams(name, streamsBuilder =>
+        {
+            streamsBuilder.ConfigureRedis(optionsBuilder => optionsBuilder.Configure<IServiceProvider>((options, services) =>
+            {
+                var serviceKey = configurationSection["ServiceKey"];
+                if (!string.IsNullOrEmpty(serviceKey))
+                {
+                    // Get a connection multiplexer instance by name.
+                    var multiplexer = services.GetRequiredKeyedService<IConnectionMultiplexer>(serviceKey);
+                    options.CreateMultiplexer = _ => Task.FromResult(multiplexer);
+                    options.ConfigurationOptions = new ConfigurationOptions();
+                }
+                else
+                {
+                    // Construct a connection multiplexer from a connection string.
+                    var connectionName = configurationSection["ConnectionName"];
+                    var connectionString = configurationSection["ConnectionString"];
+                    if (!string.IsNullOrEmpty(connectionName) && string.IsNullOrEmpty(connectionString))
+                    {
+                        var rootConfiguration = services.GetRequiredService<IConfiguration>();
+                        connectionString = rootConfiguration.GetConnectionString(connectionName);
+                    }
+
+                    if (!string.IsNullOrEmpty(connectionString))
+                    {
+                        options.ConfigurationOptions = ConfigurationOptions.Parse(connectionString);
+                    }
+                }
+            }));
+
+            if (int.TryParse(configurationSection["PartitionCount"], out var partitionCount))
+            {
+                streamsBuilder.ConfigurePartitioning(partitionCount);
+            }
+        });
+    }
+}
